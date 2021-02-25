bec9d771f33ed51fa537d7ccf4b9eb85daea3e10
mpardej <mpardej@gmail.com>
2021-02-25 16:56:27 +0100

    new subnet in MxPVpc1

--- main.tf
+++ main.tf
@@ -32,3 +32,12 @@ resource "aws_vpc" "vpc" {
     Terraform = "true"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.MxP_Vpc1.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
