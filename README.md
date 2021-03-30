d6cb5bc510c8931538efae0dd3baaa34214aced0
mpardej <mpardej@gmail.com>
2021-03-30 15:26:08 +0200

    [skip ci] repo cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AWS_Terraform
+Creates basic VPC with private & public subnets.
+Depends on 
+    terraform-aws-modules/vpc/aws
+    terraform-aws-modules/security-group/aws
--- main.tf
+++ main.tf
@@ -26,8 +26,15 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_ipv6 = true
+
+  enable_s3_endpoint       = true
+  enable_dynamodb_endpoint = true
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  map_public_ip_on_launch = false
 
   tags = {
     Terraform = "true"
@@ -55,74 +62,3 @@ module "sql_service_sg" {
 }
 
 
-# Call the seed_module to build our ADO seed info
-#module "bootstrap" {
-#  source                      = "./modules/bootstrap"
-#  name_of_s3_bucket           = "s3bucket-aws-wmakarzak01"
-#  dynamo_db_table_name        = "aws-locks-wmakarzak"
-#}
-
-# Build the VPC
-# resource "aws_vpc" "vpc" {
-#   cidr_block           = "10.1.0.0/16"
-#   instance_tenancy     = "default"
-#   tags = {
-#     Name      = "MainVPC_WM"
-#     Terraform = "true"
-#   }
-# }
-# # add subnet
-# resource "aws_subnet" "sql_sbn" {
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = "10.1.10.0/24"
-#   availability_zone = data.aws_availability_zones.available.names[0]
-
-#   tags = {
-#     Name = "sql_sbn"
-#   }
-# }
-
-# resource "aws_subnet" "sql_sbn_secondary" {
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = "10.1.20.0/24"
-#   availability_zone = data.aws_availability_zones.available.names[1]
-
-#   tags = {
-#     Name = "sql_sbn"
-#   }
-# }
-
-# resource "aws_db_subnet_group" "default" {
-#     name       = "main"
-#     subnet_ids = [aws_subnet.sql_sbn.id, aws_subnet.sql_sbn_secondary.id]
-
-#     tags = {
-#     Name = "sql_subnet"
-#   }
-# }
-
-# module "security_group" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "~> 3"
-
-#   name        = "sql-sg"
-#   description = "Complete SqlServer example security group"
-#   vpc_id      = data.aws_vpc.vpc.id
-
-#   # ingress
-#   ingress_with_cidr_blocks = [
-#     {
-#       from_port   = 1433
-#       to_port     = 1433
-#       protocol    = "tcp"
-#       description = "SqlServer access from within VPC"
-#       cidr_blocks = data.aws_vpc.vpc.cidr_block
-#     },
-#   ]
-
-# }
-
-module "db" {
-  source     = "./mssql"
-  
- }
